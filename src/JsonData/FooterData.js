9e82508ef9e423e5f7f43a3d62aeefbdf2976eb2
hoa.dinhquoc <hoa.dinhquoc@gameloft.com>
2021-06-18 22:28:11 +0700

    -clean

--- src/JsonData/FooterData.js
+++
@@ -1,6 +0,0 @@
-export default {
-    FOOTER: {
-        STUDIO_NAME: "Litte Rabbit Games",
-        TEXT: "Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.",
-    }
-}
--- src/JsonData/HeroSectionData.js
+++
@@ -1,7 +0,0 @@
-export default {
-    HERO: {
-        WELCOME: "Welcome To Little Rabbits Studio",
-        SMALL_INTRODUCTION: "We are team of talented developers making games for everyone",
-        GET_START: "Get Started"
-    }
-}
